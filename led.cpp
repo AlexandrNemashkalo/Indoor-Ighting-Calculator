f1759bbbb409c1142c3429da9a4ba02b8265a5f1
Alexandr Nemashkalo <60902736+AlexandrNemashkalo@users.noreply.github.com>
2020-10-16 21:45:27 +0300

    Update led.cpp

--- led.cpp
+++ led.cpp
@@ -7,6 +7,8 @@ float Led::DEFAULT_COLOR_VALUE = 100;
 
 Led::Led():Lamp()
 {
+    /* Конструктор по умолчанию
+     */
     this->power = Lamp::DEFAULT_POWER;
     this->intensity = Lamp::DEFAULT_INTENSITY;
     this->red =Led::DEFAULT_COLOR_VALUE;
@@ -19,6 +21,9 @@ Led::Led(float power_ = Lamp::DEFAULT_POWER, float intensity_ = Lamp::DEFAULT_IN
          float green_ = Led::DEFAULT_COLOR_VALUE,
          float blue_ = Led::DEFAULT_COLOR_VALUE) : Lamp(power_, intensity_)
 {
+    /* Конструктор инициализации
+     * Входные параметры: мощность, сила света, интенсивность коасного, зеленого и желтого света)
+     */
     Led::setBlue(blue_);
     Led::setGreen(green_);
     Led::setRed(red_);
@@ -26,6 +31,9 @@ Led::Led(float power_ = Lamp::DEFAULT_POWER, float intensity_ = Lamp::DEFAULT_IN
 
 Led::Led(const Led &led)
 {
+    /* Конструктор копирования
+     * Входные параметры:  лампа
+     */
     this->power = led.power;
     this->intensity = led.intensity;
     this->red = led.red;
@@ -40,27 +48,37 @@ Led::~Led()
 
 float Led::currIntensity(const Led & led) const
 {
+    /* Метод , который подсчитывает текущую силу света
+     */
     float currI = led.intensity * (led.red + led.green + led.blue)/(3*Led::MAX_COLOR_VALUE);
     return currI;
 }
 
 const float& Led::getRed()const
 {
+    /* Метод , возвращающий интенсивность красного света
+     */
     return this->red;
 }
 
 const float& Led::getGreen()const
 {
+    /* Метод , возвращающий интенсивность зеленого света
+     */
     return this->green;
 }
 
 const float& Led::getBlue()const
 {
+    /* Метод , возвращающий интенсивность синего света
+     */
     return this->blue;
 }
 
 void Led::setBlue(float blue_)
 {
+    /* Метод , устанавливающий значение интенсивности синего света
+     */
     if(blue_ < Led::MIN_COLOR_VALUE)
         this->blue = Led::MIN_COLOR_VALUE;
     else if(blue_ > Led::MAX_COLOR_VALUE)
@@ -71,6 +89,8 @@ void Led::setBlue(float blue_)
 
 void Led::setGreen(float green_)
 {
+    /* Метод , устанавливающий значение интенсивности зеленого света
+     */
     if(green_ < Led::MIN_COLOR_VALUE)
         this->green = Led::MIN_COLOR_VALUE;
     else if(green_ > Led::MAX_COLOR_VALUE)
@@ -81,6 +101,8 @@ void Led::setGreen(float green_)
 
 void Led::setRed(float red_)
 {
+    /* Метод , устанавливающий значение интенсивности красного света
+     */
     if(red_ < Led::MIN_COLOR_VALUE)
         this->red = Led::MIN_COLOR_VALUE;
     else if(red_ > Led::MAX_COLOR_VALUE)
@@ -91,6 +113,8 @@ void Led::setRed(float red_)
 
 LampType Led::getType()
 {
+    /* Метод , возвращающий тип источника света
+     */
     return led;
 }
 
